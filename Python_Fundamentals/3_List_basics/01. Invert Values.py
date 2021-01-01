871fe5fd4097bc12db9bbf7f53c80a32ad396e0a
Kalin Petrov <39294276+kalinpetrovbg@users.noreply.github.com>
2021-01-01 13:03:27 +0200

    Create 01. Invert Values.py

---
+++ Python_Fundamentals/3_List_basics/01. Invert Values.py
@@ -0,0 +1,13 @@
+# numbers = input().split(" ")
+# num = []
+# for each in numbers:
+#     int_each = int(each)
+#     int_each *= -1
+#     num.append(int_each)
+# print(num)
+
+numbers = input().split(" ")
+num = []
+for each in numbers:
+    num.append(-int(each))
+print(num)
