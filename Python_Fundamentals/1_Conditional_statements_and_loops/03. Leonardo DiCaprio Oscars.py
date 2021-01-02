5354f101ae3d02a2c35412ea9c11451bd3b222bf
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-01-02 23:00:24 +0200

    uploading solutions

---
+++ Python_Fundamentals/1_Conditional_statements_and_loops/03. Leonardo DiCaprio Oscars.py
@@ -0,0 +1,10 @@
+num = int(input())
+
+if num == 88:
+    print("Leo finally won the Oscar! Leo is happy")
+elif num == 86:
+    print("Not even for Wolf of Wall Street?!")
+elif num < 88 and num != 86:
+    print("When will you give Leo an Oscar?")
+else:
+    print("Leo got one already!")
