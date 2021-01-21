1c021691787895cf2bb0ed20199c084278551489
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-01-21 23:26:17 +0200

    update

--- Python Advanced/Multidimensional Lists - Lab/01. Sum Matrix Elements.py
+++ Python Advanced/Multidimensional Lists - Lab/01. Sum Matrix Elements.py
@@ -1,4 +1,16 @@
-matrix = [str(x) for x in input().split(",")]
-for i in range(len(matrix)):
-    for j in range(len(matrix[i])):
-        print(matrix[i][j])
+rows, cols = [int(x) for x in input().split(", ")]
+
+matrix = []
+for x in range(rows):
+    row = [int(x) for x in input().split(",")]
+    matrix.append(row)
+
+matrix_sum = 0
+
+for r in range(len(matrix)):
+    row = matrix[r]
+    for c in range(len(row)):
+        matrix_sum += row[c]
+
+print(matrix_sum)
+print(matrix)
