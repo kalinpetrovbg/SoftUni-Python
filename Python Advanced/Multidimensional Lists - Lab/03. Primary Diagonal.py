83083529acfecf657a584e50e1040ebd02c84926
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-01-23 12:29:34 +0200

    update

--- Python Advanced/Multidimensional Lists - Lab/03. Primary Diagonal.py
+++ Python Advanced/Multidimensional Lists - Lab/03. Primary Diagonal.py
@@ -1,12 +1,4 @@
 n = int(input())
-matrix = []
-for _ in range(n):
-    line = [int(x) for x in input().split()]
-    matrix.append(line)
-
-diagonal_sum = 0
-
-for i in range(len(matrix)):
-    diagonal_sum += matrix[i][i]
-
-print(diagonal_sum)
+matrix = [[int(x) for x in input().split()] for _ in range(n)]
+value = sum([matrix[i][i] for i in range(n)])
+print(value)
