3b2a5abe1ea3fe711bb0ac49ddf16015817d67c9
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-01-23 12:55:23 +0200

    update

--- Python Advanced/Multidimensional Lists - Lab/04. Symbol in Matrix.py
+++ Python Advanced/Multidimensional Lists - Lab/04. Symbol in Matrix.py
@@ -1,39 +1,15 @@
-def read_matrix(is_test=False):
-    if is_test:
-        return [
-            ['A', 'B', 'C'],
-            ['D', 'E', 'F'],
-            ['X', '!', '@'],
-        ]
-    else:
-        size = int(input())
-        matrix = []
-        for row_index in range(size):
-            row = [x for x in input()]
-            matrix.append(row)
+def find_in_matrix(matrix, search):
+    for i, row in enumerate(matrix):
+        for j, value in enumerate(row):
+            if value == search:
+                return i, j
 
-        return matrix
+n = int(input())
+matrix = [list(input()) for _ in range(n)]
+search = input()
 
-
-def find_symbol_in_matrix(matrix, symbol):
-    for row_index in range(len(matrix)):
-        if symbol in matrix[row_index]:
-            return row_index, matrix[row_index].index(symbol)
-        # for col_index in range(len(matrix[row_index])):
-        #     current_symbol = matrix[row_index][col_index]
-        #     if current_symbol == symbol:
-        #         return (row_index, col_index)
-    return None
-
-
-def print_result(result, symbol):
-    if result:
-        print(result)
-    else:
-        print(f'{symbol} does not occur in the matrix')
-
-
-matrix = read_matrix(is_test=True)
-symbol = '1'
-
-print_result(find_symbol_in_matrix(matrix, symbol), symbol)
+position = find_in_matrix(matrix, search)
+if position:
+    print(position)
+else:
+    print(f"{search} does not occur in the matrix")
