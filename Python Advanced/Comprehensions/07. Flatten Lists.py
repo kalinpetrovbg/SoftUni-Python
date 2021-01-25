4c391ca770dcca9c1a5608c96c36377eebc1a8db
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-01-25 23:35:39 +0200

    update

--- Python Advanced/Comprehensions/07. Flatten Lists.py
+++ Python Advanced/Comprehensions/07. Flatten Lists.py
@@ -1,3 +1,8 @@
-numbers = [x for x in input().replace(" ", "").split("|")]
-result = [num for num in numbers[::-1]]
-print(' '.join("".join(str(x) for x in result)))
+# numbers = [x for x in input().replace(" ", "").split("|")]
+# result = [num for num in numbers[::-1]]
+# print(' '.join("".join(str(x) for x in result)))
+
+data = input().split("|")
+data.reverse()
+result = [value.strip() for i in range(len(data)) for value in data[i].split()]
+print(*result)
