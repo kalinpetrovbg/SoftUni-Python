9445f3d95c91dbe6a71c60c7f89d89208a99bcd6
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-01-27 23:44:57 +0200

    update

--- Python Advanced/Comprehensions/09. Bunker.py
+++ Python Advanced/Comprehensions/09. Bunker.py
@@ -1,30 +1,17 @@
-categories = input().split(", ")
-elements = {cat: {} for cat in categories}
+elements = {cat: [] for cat in input().split(", ")}
 n = int(input())
 total_qty = 0
 total_quality = 0
-nums = 0
 
 for _ in range(n):
     command = input()
     bundle = command.replace(";", " - ")
-
     cat, item, qty, qua = bundle.split(" - ")
-    nums += 1
-    _, quantity = qty.split(":")
-    quantity = int(quantity)
-    _, quality = qua.split(":")
-    quality = int(quality)
-    total_qty += quantity
-    total_quality += quality        # potential error
-
-    if not elements[cat]:
-        elements[cat] = []
-        elements[cat].append(item)
-    else:
-        elements[cat].append(item)
+    total_qty += int(qty.split(":")[1])
+    total_quality += int(qua.split(":")[1])
+    elements[cat].append(item)
 
 print(f"Count of items: {total_qty}")
-print(f"Average quality: {total_quality / len(categories):.2f}")
+print(f"Average quality: {total_quality / len(elements):.2f}")
 for category, types in elements.items():
     print(f"{category} -> {', '.join(types)}")
