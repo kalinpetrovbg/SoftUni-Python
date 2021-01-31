5eee6f7374ed9426256834726c73bb8f6ee8d53e
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-01-31 17:06:50 +0200

    update

--- Python-Advanced/Multidimensional-Lists/04. Matrix Shuffling.py
+++ Python-Advanced/Multidimensional-Lists/04. Matrix Shuffling.py
@@ -1,6 +1,5 @@
-rows, cols = [int(x) for x in input().split()]
-matrix = [input().split() for _ in range(rows)]
-
+row, col = [int(x) for x in input().split()]
+matrix = [[x for x in input().split()] for _ in range(row)]
 command = input().split()
 
 while command[0] != "END":
@@ -8,14 +7,15 @@ while command[0] != "END":
     if command[0] != "swap" or len(command) != 5:
         print("Invalid input!")
     else:
-        a, b, c, d = [int(x) for x in command[1:]]
-        if a > rows - 1 or c > rows - 1 or b > cols - 1 or d > cols - 1:
-            print("Invalid input!")
-        else:
-            temp = matrix[int(a)][int(b)]
-            matrix[int(a)][int(b)] = matrix[int(c)][int(d)]
-            matrix[int(c)][int(d)] = temp
+        old_row, old_col = int(command[1]), int(command[2])
+        new_row, new_col = int(command[3]), int(command[4])
 
+        if 0 <= old_row < row and 0 <= old_col < col and 0 <= new_row < row and 0 <= new_col < col:
+            old_number = matrix[old_row][old_col]
+            matrix[old_row][old_col] = matrix[new_row][new_col]
+            matrix[new_row][new_col] = old_number
             print("\n".join(' '.join(map(str, x)) for x in matrix))
+        else:
+            print("Invalid input!")
 
     command = input().split()
