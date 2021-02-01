638201dcbd7069555ccfbaac38ccce8972246d77
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-02-01 10:56:13 +0200

    update

--- Python-Advanced/Multidimensional-Lists/06. Knight Game - no functions.py
+++ Python-Advanced/Multidimensional-Lists/06. Knight Game - no functions.py
@@ -1,44 +1,27 @@
 n = int(input())
-matrix = [list(input()) for _ in range(n)]
+matrix = [[x for x in list(input())] for _ in range(n)]
+removed = 0
+hit_row = (-2, -2, +2, +2, -1, -1, +1, +1)
+hit_col = (-1, +1, -1, +1, -2, +2, +2, -2)
 
-best_knight_killer = False
-any_kills = True
-kills = 0
-most_kills = 0
-hits = [(-2, 1), (-1, 2), (1, 2), (2, 1), (2, -1), (1, -2), (-1, -2), (-2, -1)]
-removed_knights = 0
-
-while any_kills:
-    for row in range(n):
-        for col in range(n):
-            if matrix[row][col] == "K":
-                for i in range(8):
-                    new_row = row + hits[i][0]
-                    new_col = col + hits[i][1]
-                    if new_row < 0 or new_col < 0 or new_row > n - 1 or new_col > n - 1:
-                        continue
-                    else:
-                        new_position = matrix[row + hits[i][0]][col + hits[i][1]]
-                        if new_position == "K":
-                            kills += 1
-                            any_kills = True
-
-                if kills > most_kills:
-                    most_kills = kills
-                    best_knight_killer = matrix[row][col]
-                    update = row, col
+while True:
+    best_killer = 0
+    monster = []
+    for i in range(n):
+        for j in range(n):
+            if matrix[i][j] == "K":
                 kills = 0
-
-    if best_knight_killer:
-        best_knight_killer = 0
-        matrix[update[0]][update[1]] = 0
-        if most_kills != 0:
-            removed_knights += 1
+                for x in range(8):
+                    if 0 <= i + hit_row[x] < n and 0 <= j + hit_col[x] < n:
+                        if matrix[i + hit_row[x]][j + hit_col[x]] == "K":
+                            kills += 1
+                if best_killer < kills:
+                    monster = [i, j]
+                    best_killer = kills
+    if monster:
+        matrix[monster[0]][monster[1]] = 0
+        removed += 1
     else:
-        any_kills = False
         break
 
-print("-----------")
-print("\n".join(''.join(map(str, x)) for x in matrix))
-
-print(removed_knights)
+print(removed)
