dd2855d7bf984b060c397f81187f9e7b02e2fef6
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-02-02 14:04:50 +0200

    update temp

--- Python-Advanced/Multidimensional-Lists/09. Radioactive Mutate Vampire Bunnies.py
+++ Python-Advanced/Multidimensional-Lists/09. Radioactive Mutate Vampire Bunnies.py
@@ -2,31 +2,65 @@ rows, cols = [int(x) for x in input().split()]
 matrix = [list(input()) for _ in range(rows)]
 commands = list(input())
 pos = []
+dead = False
 
 for i in range(rows):
     for j in range(cols):
         if matrix[i][j] == "P":
             pos.append(i)
             pos.append(j)
-print(pos)
+            matrix[i][j] = "."
 
 while commands:
+
     if commands[0] == "U":
-        if 0 <= pos[0] - 1 < rows and 0 <= pos[1] < cols:
-            pos[0] -= 1
-
+        pos[0] -= 1
     elif commands[0] == "D":
-        if 0 <= pos[0] + 1 < rows and 0 <= pos[1] < cols:
-            pos[0] += 1
-
+        pos[0] += 1
     elif commands[0] == "L":
-        if 0 <= pos[0] < rows and 0 <= pos[1] - 1 < cols:
-            pos[1] -= 1
-
+        pos[1] -= 1
     elif commands[0] == "R":
-        if 0 <= pos[0] < rows and 0 <= pos[1] + 1 < cols:
+        pos[1] += 1
+
+    for y in range(rows):
+        for z in range(cols):
+            if matrix[y][z] == "B":
+                if 0 <= y+1 < rows and 0 <= z < cols:
+                    matrix[y+1][z] = "C"
+                if 0 <= y - 1 < rows and 0 <= z < cols:
+                    matrix[y-1][z] = "C"
+                if 0 <= y < rows and 0 <= z + 1 < cols:
+                    matrix[y][z+1] = "C"
+                if 0 <= y < rows and 0 <= z - 1 < cols:
+                    matrix[y][z-1] = "C"
+
+    for i in range(rows):
+        for j in range(cols):
+            if matrix[i][j] == "C":
+                matrix[i][j] = "B"
+
+
+
+    if 0 > pos[0] or 0 > pos[1] or pos[0] >= rows or pos[1] >= cols:
+        if commands[0] == "U":
+            pos[0] += 1
+        elif commands[0] == "D":
+            pos[0] -= 1
+        elif commands[0] == "L":
             pos[1] += 1
+        elif commands[0] == "R":
+            pos[1] -= 1
+        dead = False
+        break
 
-
+    if matrix[pos[0]][pos[1]] == "B":
+        dead = True
+        break
 
     commands.pop(0)
+
+print("\n".join("".join(map(str, x)) for x in matrix))
+if not dead:
+    print(f"won: {pos[0]} {pos[1]}")
+else:
+    print(f"dead: {pos[0]} {pos[1]}")
