91a904c69619369821db8547021f22ad7d6373ff
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-02-03 22:02:50 +0200

    uploading solutions

---
+++ Python-Advanced/Advanced-Exams/16 Dec 2020/Problem 3.py
@@ -0,0 +1,18 @@
+def get_magic_triangle(n_rows):
+    results = [] # a container to collect the rows
+    for _ in range(n_rows):
+        row = [1] # a starter 1 in the row
+        if results: # then we're in the second row or beyond
+            last_row = results[-1] # reference the previous row
+            # this is the complicated part, it relies on the fact that zip
+            # stops at the shortest iterable, so for the second row, we have
+            # nothing in this list comprehension, but the third row sums 1 and 1
+            # and the fourth row sums in pairs. It's a sliding window.
+            row.extend([sum(pair) for pair in zip(last_row, last_row[1:])])
+            # finally append the final 1 to the outside
+            row.append(1)
+        results.append(row) # add the row to the results.
+    return results
+
+num = 5
+print(get_magic_triangle(num))
