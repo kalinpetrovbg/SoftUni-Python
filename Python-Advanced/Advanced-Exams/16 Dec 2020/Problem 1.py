5d13b514e819888746e1e2624800e88f517bff59
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-02-03 14:11:38 +0200

    update solutions

--- Python-Advanced/Advanced-Exams/16 Dec 2020/Problem 1.py
+++ Python-Advanced/Advanced-Exams/16 Dec 2020/Problem 1.py
@@ -1,46 +1,39 @@
-males = [int(x) for x in input().split(" ") if int(x) != 0]
-females = [int(x) for x in input().split(" ") if int(x) != 0]
+males = [int(x) for x in input().split(" ") if int(x) > 0][::-1]
+females = [int(x) for x in input().split(" ") if int(x) > 0]
 matches = 0
-fem = 0
-man = 0
 
-while True:
+for index in range(len(males)):
+    if males[index] % 25 == 0:
+        males[index] = -1
+        if index + 1 < len(males):
+            males[index + 1] = -1
+males = [x for x in males if x != -1]
 
-    for index in range(len(females)):
-        if females[index] % 25 == 0 and females[index] != 0:
-            females[index] = 0
-            if 0 < index < len(females):
-                females[index + 1] = 0
+for index in range(len(females)):
+    if females[index] % 25 == 0:
+        females[index] = -1
+        if index + 1 < len(females):
+            females[index + 1] = -1
+females = [x for x in females if x != -1]
 
-    for index in range(len(males)):
-        if males[index] % 25 == 0 and males[index] != 0:
-            males[index] = 0
-            if 0 < index < len(males):
-                males[index + 1] = 0
+while males and females:
 
-    females = list(filter(lambda x: x != 0, females))
-    males = list(filter(lambda x: x != 0, males))
-
-    if females[0] == males[-1]:
+    if females[0] == males[0]:
         females.pop(0)
-        males.pop()
+        males.pop(0)
         matches += 1
     else:
         females.pop(0)
-        males[-1] -= 2
-        if males[-1] <= 0:
-            males.pop()
-
-    if not males:
-        break
-    if not females:
-        break
-
-if len(females) == 0:
-    females.append("none")
-if len(males) == 0:
-    males.append("none")
+        males[0] -= 2
+        if males[0] <= 0:
+            males.pop(0)
 
 print(f"Matches: {matches}")
-print(f"Males left: {', '.join(map(str, males[::-1]))}")
-print(f"Females left: {', '.join(map(str, females))}")
+if males:
+    print(f"Males left: {', '.join(map(str, males))}")
+else:
+    print(f"Males left: none")
+if females:
+    print(f"Females left: {', '.join(map(str, females))}")
+else:
+    print(f"Females left: none")
