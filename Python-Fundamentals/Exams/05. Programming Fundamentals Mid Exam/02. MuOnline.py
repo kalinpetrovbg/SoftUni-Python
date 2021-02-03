dd88e7733fba8276b00397928b18bff4a52b0bd4
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-02-03 22:08:31 +0200

    uploading solutions

---
+++ Python-Fundamentals/Exams/05. Programming Fundamentals Mid Exam/02. MuOnline.py
@@ -0,0 +1,38 @@
+rooms = input().split("|")
+health = 100
+bitcoins = 0
+room = 0
+
+for each in rooms:
+    command, number = each.split(" ")
+    number = int(number)
+    room += 1
+
+    if command == "potion":
+        old_health = health
+        health += number
+        if health >= 100:
+            print(f"You healed for {100 - old_health} hp.")
+            health = 100
+        else:
+            print(f"You healed for {number} hp.")
+        print(f"Current health: {health} hp.")
+
+    elif command == "chest":
+        bitcoins += number
+        print(f"You found {number} bitcoins.")
+
+    else:
+        health -= number
+        if health > 0:
+            print(f"You slayed {command}.")
+        else:
+            print(f"You died! Killed by {command}.")
+            print(f"Best room: {room}")
+            break
+
+if health > 0:
+    print(f"You\'ve made it!")
+    print(f"Bitcoins: {bitcoins}")
+    print(f"Health: {health}")
+
