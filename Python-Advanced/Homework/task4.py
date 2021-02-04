f67d700dc6960636fcf0d76f708356663897f5d4
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-02-04 15:39:30 +0200

    update homework

--- Python-Advanced/Homework/task4.py
+++ Python-Advanced/Homework/task4.py
@@ -1,16 +1,24 @@
 import os
+result = ""
 
-files = [f for f in os.listdir('.') if os.path.isfile(f)]
-dict = {}
-for file in files:
-    name, ending = file.split(".")
-    if ending not in dict:
-        dict[ending] = []
-        dict[ending].append(name)
+file_list = [f for f in os.listdir('.') if os.path.isfile(f)]
+files = {}
+
+for each in file_list:
+    name, ending = each.split(".")
+    if ending not in files:
+        files[ending] = []
+        files[ending].append(name)
     else:
-        dict[ending].append(name)
+        files[ending].append(name)
 
-for types, name in dict.items(), sorted(lambda x: x[1]):
-    print(f".{types}")
-    full_names = ["- - - " + x + "." + types for x in name]
-    print("\n".join(full_names))
+files = sorted(files.items(), key=lambda x: (x[0], x[1]))
+desktop = os.path.join(os.path.join(os.environ['USERPROFILE'], "Desktop"), "report.txt")
+writing = open(desktop, "w")
+
+for types, files in files:
+    print("." + types, file=writing)
+    for file in files:
+        print(f"- - - {file}.{types}", file=writing)
+
+
