b0c8d740ea2a0a26c98d5a074ee5e0fa640fc0de
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-02-05 22:35:52 +0200

    updating solutions

--- Python-Fundamentals/Exams/01. Programming Fundamentals Final Exam Retake/01. The Imitation Game.py
+++ Python-Fundamentals/Exams/01. Programming Fundamentals Final Exam Retake/01. The Imitation Game.py
@@ -1,1 +1,18 @@
-students = input()
+message = input()
+text = input()
+
+while text != "Decode":
+    command = text.split("|")
+    if command[0] == "Move":
+        index = int(command[1])
+        message = message[index:] + message[:index]
+    elif command[0] == "Insert":
+        index = int(command[1])
+        ending = message[index:]
+        message = message[:index] + command[2] + ending
+    elif command[0] == "ChangeAll":
+        temp = str.maketrans(command[1], command[2])
+        message = message.translate(temp)
+    text = input()
+
+print(f"The decrypted message is: {message}")
