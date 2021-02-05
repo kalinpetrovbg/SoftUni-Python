39871983f6aa61aa0be2c5df31e8d316542a2a3e
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-02-05 21:58:54 +0200

    better solutions

--- Python-Advanced/List-as-Stacks-and-Queues/06. Balanced Parentheses.py
+++ Python-Advanced/List-as-Stacks-and-Queues/06. Balanced Parentheses.py
@@ -1,44 +1,27 @@
 text = input()
-first = []
-second = []
-check = True
+stack = []
+checker = True
+mapper = {
+    "}": "{",
+    "]": "[",
+    ")": "(",
+}
 
-for each in text:
-    first.append(each)
-    if each == ")":
-        if second:
-            if second[-1] != "(":
-                check = False
-                break
+for el in text:
+    if el in "{[(":
+        stack.append(el)
+    else:
+        if stack:
+            if stack[-1] == mapper[el]:
+                stack.pop()
             else:
-                second.pop()
-        else:
-            check = False
-            break
-    elif each == "}":
-        if second:
-            if second[-1] != "{":
-                check = False
+                checker = False
                 break
-            else:
-                second.pop()
         else:
-            check = False
+            checker = False
             break
-    elif each == "]":
-        if second:
-            if second[-1] != "[":
-                check = False
-                break
-            else:
-                second.pop()
-        else:
-            check = False
-            break
-    elif each == "(" or each == "{" or each == "[":
-        second.append(each)
 
-if check == True:
-    print(f"YES")
+if checker:
+    print("YES")
 else:
-    print(f"NO")
+    print("NO")
