89c2fcb5e527abb68923c6e9a218c3e2d5d5a95f
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-02-06 19:07:42 +0200

    uploading solutions

---
+++ Python-Fundamentals/Exams/03. Programming Fundamentals Mid Exam Retake/01. Counter Strike.py
@@ -0,0 +1,23 @@
+energy = int(input())
+battles = 0
+
+while True:
+
+    distance = input()
+
+    if True:
+        if distance == "End of battle":
+            print(f"Won battles: {battles}. Energy left: {energy}")
+            break
+        else:
+            distance = int(distance)
+
+    if True:
+        if energy - distance >= 0:
+            energy -= distance
+            battles += 1
+            if battles % 3 == 0:
+                energy += battles
+        else:
+            print(f"Not enough energy! Game ends with {battles} won battles and {energy} energy")
+            break
