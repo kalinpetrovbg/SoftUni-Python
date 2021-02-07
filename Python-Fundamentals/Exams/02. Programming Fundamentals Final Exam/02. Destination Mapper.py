8766e77b170463e40aa00c6cf57362b53fbbc402
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-02-07 09:53:04 +0200

    update

--- Python-Fundamentals/Exams/02. Programming Fundamentals Final Exam/02. Destination Mapper.py
+++ Python-Fundamentals/Exams/02. Programming Fundamentals Final Exam/02. Destination Mapper.py
@@ -1,12 +1,10 @@
 import re
 
 pattern = r"(=|/)([A-Z][a-zA-Z]{2,})(\1)"
-
 the_string = input()
-
 result = re.finditer(pattern, the_string)
-
 all_destinations= []
+
 for i in result:
     dirty = i[0]
     cleaned_dest = dirty[1: -1]
