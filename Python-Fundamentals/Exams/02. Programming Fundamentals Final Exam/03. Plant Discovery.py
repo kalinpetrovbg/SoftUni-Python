c662e02ef217252a2bcdb04a7755054aa13ac43b
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-02-07 02:35:57 +0200

    uploading solutions

---
+++ Python-Fundamentals/Exams/02. Programming Fundamentals Final Exam/03. Plant Discovery.py
@@ -0,0 +1,51 @@
+n = int(input())
+plants = {}
+new_plants = {}
+for _ in range(n):
+    name, rare = input().split("<->")
+    plants[name] = [int(rare)]
+
+data = input()
+
+while data != "Exhibition":
+    data = data.replace(" - ", ": ")
+
+    if "Rate" in data:
+        _, plant, rating = data.split(": ")
+        if plant in plants and int(rating) > 0:
+            plants[plant].append(float(rating))
+        else:
+            print(f"error")
+
+    elif "Update" in data:
+        _, plant, rarity = data.split(": ")
+        if plant in plants and int(rarity) > 0:
+            plants[plant][0] = int(rarity)
+        else:
+            print(f"error")
+
+    elif "Reset" in data:
+        _, plant = data.split(": ")
+        if plant in plants:
+            try:
+                del plants[plant][1]
+            except:
+                print(f"error")
+        else:
+            print(f"error")
+
+    else:
+        print(f"error")
+
+    data = input()
+
+for plant, stats in plants.items():
+    if len(stats) > 1:
+        rating = sum(stats[1:]) / (len(stats) - 1)
+        new_plants[plant] = [stats[0], rating]
+    else:
+        new_plants[plant] = [stats[0], 0.00]
+
+print("Plants for the exhibition:")
+for plant, stats in sorted(new_plants.items(), key=lambda x: (-x[1][0], -x[1][1])):
+    print(f"- {plant}; Rarity: {stats[0]}; Rating: {stats[1]:.2f}")
