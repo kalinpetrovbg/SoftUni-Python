6dda763b745930d7b717ea614873ec8ea627dbb3
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-02-07 10:30:15 +0200

    update

--- Python-Fundamentals/Exams/02. Programming Fundamentals Mid Exam/03. Numbers.py
+++ Python-Fundamentals/Exams/02. Programming Fundamentals Mid Exam/03. Numbers.py
@@ -1,14 +1,8 @@
 numbers = [int(el) for el in input().split()]
 average = sum(numbers) / len(numbers)
-new_list = []
+new_list = sorted([num for num in numbers if num > average], reverse=True)
 
-for num in numbers:
-    if num > average:
-        new_list.append(num)
-
-good_list = sorted(new_list, reverse=True)
-
-if len(good_list) > 0:
-    print(*good_list[:5], sep=" ")
+if len(new_list) > 0:
+    print(*new_list[:5], sep=" ")
 else:
     print("No")
