2f15b3537ea8c77ecad6266bfdda1500d4ea626c
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-02-08 00:31:03 +0200

    update

--- Python-Advanced/List-as-Stacks-and-Queues/09. Key Revolver.py
+++ Python-Advanced/List-as-Stacks-and-Queues/09. Key Revolver.py
@@ -1,31 +1,28 @@
 from collections import deque
 
-bullet_price = int(input())
-size_of_barrel = int(input())
+price = int(input())
+barrel = int(input())
 bullets = [int(x) for x in input().split()]
-lockers = deque([int(y) for y in input().split()])
+locks = deque([int(x) for x in input().split()])
 intelligence = int(input())
-total_shots = 0
+shots = 0
 
-while bullets:
-    if lockers:
-        bullet = bullets.pop()
-        total_shots += 1
+while locks and bullets:
+    current_bullet = bullets.pop()
+    current_lock = locks.popleft()
+    shots += 1
 
-        if bullet <= lockers[0]:
-            lockers.popleft()
-            print("Bang!")
-        else:
-            print("Ping!")
-
-        if bullets:
-            if total_shots % size_of_barrel == 0:
-                print("Reloading!")
+    if current_bullet > current_lock:
+        print("Ping!")
+        locks.appendleft(current_lock)
     else:
-        break
+        print("Bang!")
 
-if not lockers:
-    bullet_cost = total_shots * bullet_price
-    print(f"{len(bullets)} bullets left. Earned ${intelligence - bullet_cost}")
-else:
-    print(f"Couldn't get through. Locks left: {len(lockers)}")
+    if shots % barrel == 0 and bullets:
+        print("Reloading!")
+        c_barrel = barrel
+
+if not locks:
+    print(f"{len(bullets)} bullets left. Earned ${intelligence - (shots * price)}")
+elif not bullets:
+    print(f"Couldn't get through. Locks left: {len(locks)}")
