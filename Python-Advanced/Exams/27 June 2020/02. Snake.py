de3816db0ffa7b0235f121a337e93c219af12bad
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-02-13 14:38:28 +0200

    update

--- Python-Advanced/Exams/27 June 2020/02. Snake.py
+++ Python-Advanced/Exams/27 June 2020/02. Snake.py
@@ -1,103 +1,98 @@
 n = int(input())
 matrix = [list(input()) for _ in range(n)]
 food = 0
-holes = []
 dead = False
+clear = False
+found_b = False
 
-while True:
+while food != 10:
 
-    if dead == True:
+    if dead:
         break
 
     command = input()
+    move = False
 
     for i in range(n):
+        if move:
+            break
+        if dead:
+            break
         for j in range(n):
+
+            while found_b:
+                for y in range(n):
+                    for z in range(n):
+                        if matrix[y][z] == "B":
+                            matrix[y][z] = "S"
+                            found_b = False
+                            move = True
+                            continue
+            if move:
+                break
+            move = False
+
             if matrix[i][j] == "S":
+                matrix[i][j] = "."
 
                 if command == "right":
                     if j + 1 < n:
-                        matrix[i][j] = "."
                         if matrix[i][j + 1] == "*":
                             food += 1
                         elif matrix[i][j + 1] == "B":
-                            matrix[i][j] = "-"
                             matrix[i][j + 1] = "."
-
-                            for y in range(n):
-                                for z in range(n):
-                                    if matrix[y][z] == "B":
-                                        matrix[y][z] = "S"
-                                        continue
+                            found_b = True
+                            break
                         matrix[i][j + 1] = "S"
-                        break
-                    else:
-                        print(f"Game over!")
-                        dead = True
+                        move = True
                         break
 
                 elif command == "left":
                     if j - 1 >= 0:
-                        matrix[i][j] = "."
                         if matrix[i][j - 1] == "*":
                             food += 1
                         elif matrix[i][j - 1] == "B":
-                            matrix[i][j] = "-"
                             matrix[i][j - 1] = "."
-
-                            for y in range(n):
-                                for z in range(n):
-                                    if matrix[y][z] == "B":
-                                        matrix[y][z] = "S"
-                                        continue
+                            found_b = True
+                            break
                         matrix[i][j - 1] = "S"
-                        break
-                    else:
-                        print(f"Game over!")
-                        dead = True
+                        move = True
                         break
 
                 elif command == "down":
                     if i + 1 < n:
-                        matrix[i][j] = "."
                         if matrix[i + 1][j] == "*":
                             food += 1
                         elif matrix[i + 1][j] == "B":
-                            matrix[i][j] = "-"
                             matrix[i + 1][j] = "."
-
-                            for y in range(n):
-                                for z in range(n):
-                                    if matrix[y][z] == "B":
-                                        matrix[y][z] = "S"
-                                        break
+                            found_b = True
+                            break
                         matrix[i + 1][j] = "S"
-                        break
-                    else:
-                        print(f"Game over!")
-                        dead = True
+                        move = True
                         break
 
                 elif command == "up":
-                    if i - 1 <= 0:
-                        matrix[i][j] = "."
+                    if i - 1 >= 0:
                         if matrix[i - 1][j] == "*":
                             food += 1
                         elif matrix[i - 1][j] == "B":
-                            matrix[i][j] = "-"
                             matrix[i - 1][j] = "."
-
-                            for y in range(n):
-                                for z in range(n):
-                                    if matrix[y][z] == "B":
-                                        matrix[y][z] = "S"
-                                        continue
+                            found_b = True
+                            break
                         matrix[i - 1][j] = "S"
-                        break
-                    else:
-                        print(f"Game over!")
-                        dead = True
+                        move = True
                         break
 
+                if not move:
+                    print(f"Game over!")
+                    dead = True
+                    break
 
+                if food == 10:
+                    move = True
+                    break
+
+if food == 10:
+    print("You won! You fed the snake.")
+print(f"Food eaten: {food}")
 print("\n".join(''.join(map(str, x)) for x in matrix))
