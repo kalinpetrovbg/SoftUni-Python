350ac372e644df42cc3742f3f634f20d653614d5
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-02-13 19:32:31 +0200

    update

--- Python-Fundamentals/Text-Processing/Text-Processing - More/02. Ascii Sumator.py
+++ Python-Fundamentals/Text-Processing/Text-Processing - More/02. Ascii Sumator.py
@@ -1,5 +1,5 @@
 start = ord(input())
 end = ord(input())
 text = [ord(x) for x in input()]
-total = sum([x for x in text if x in range(start, end)])
+total = sum([x for x in text if x in range(start + 1, end)])
 print(total)
