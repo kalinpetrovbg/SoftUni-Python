49e58567cac1f175ff700a9de9fc3f5408bf383f
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-02-17 23:48:03 +0200

    update

--- Python-Fundamentals/Dictionaries/Dictionaries - More/02. Judge.py
+++ Python-Fundamentals/Dictionaries/Dictionaries - More/02. Judge.py
@@ -7,12 +7,13 @@ while data != "no more time":
     username, contest, points = data.split(" -> ")
     points = int(points)
 
-    if username not in totals:
-        totals[username] = points
-    else:
-        totals[username] += points
-
     if contest not in results:
+
+        if username not in totals:
+            totals[username] = points
+        else:
+            totals[username] += points
+
         if username not in results:
             results[contest] = {username: points}
         else:
@@ -20,12 +21,20 @@ while data != "no more time":
                 results[contest][username] = points
     else:
         results[contest][username] = points
+        if username not in totals:
+            totals[username] = points
+        else:
+            if totals[username] < points:
+                totals[username] += points
 
     data = input()
 
-
-
-
+for course, stats in results.items():
+    print(f"{course}: {len(stats)} participants")
+    count = 0
+    for name, res in sorted(stats.items(), key=lambda x: -x[1]):
+        count += 1
+        print(f"{count}. {name} <::> {res}")
 
 print("Individual standings:")
 nums = 0
