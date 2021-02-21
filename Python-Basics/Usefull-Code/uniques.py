8757f796761f72fba17f53dd1b87e16cb57ac7aa
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-02-21 23:11:07 +0200

    update

--- Python-Basics/Usefull-Code/uniques.py
+++
@@ -1,5 +0,0 @@
-original = [1, 2, 3, 2, 1, 4, 2, 1]
-
-unique = sorted(set(original), key=original.index)
-
-print(unique)
--- Python-Basics/Usefull-Code/zip.py
+++
@@ -1,7 +0,0 @@
-list_1 = [1, 2, 3]
-list_2 = [4, 5, 6]
-list_3 = [7, 8, 9]
-
-print(list(zip(list_1, list_2, list_3)))
-
-#this prints [(1, 4, 7), (2, 5, 8), (3, 6, 9)]
