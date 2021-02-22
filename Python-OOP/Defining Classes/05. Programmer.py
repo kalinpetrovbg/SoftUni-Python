b54f62aae09137d2553c0930e1346dc7b094ce9a
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-02-22 22:46:37 +0200

    uploading solutions

---
+++ Python-OOP/Defining Classes/05. Programmer.py
@@ -0,0 +1,32 @@
+class Programmer:
+    def __init__(self, name, language, skills):
+        self.name = name
+        self.language = language
+        self.skills = skills
+
+    def watch_course(self, course_name, language, skills_earned):
+        if self.language == language:
+            self.skills += skills_earned
+            return f"{self.name} watched {course_name}"
+        else:
+            return f"{self.name} does not know {language}"
+
+    def change_language(self, new_language, skills_needed):
+        if new_language == self.language:
+            return f"{self.name} already knows {new_language}"
+        else:
+            if self.skills >= skills_needed:
+                old_language = self.language
+                self.language = new_language
+                return f"{self.name} switched from {old_language} to {new_language}"
+            else:
+                return f"{self.name} needs {skills_needed - self.skills} more skills"
+
+
+programmer = Programmer("John", "Java", 50)
+print(programmer.watch_course("Python Masterclass", "Python", 84))
+print(programmer.change_language("Java", 30))
+print(programmer.change_language("Python", 100))
+print(programmer.watch_course("Java: zero to hero", "Java", 50))
+print(programmer.change_language("Python", 100))
+print(programmer.watch_course("Python Masterclass", "Python", 84))
