9e472c63151a9f43388dc24c28978e423eec81a0
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-02-23 23:02:46 +0200

    uploading solutions

---
+++ Python-OOP/Classes-and-instances/03. Account.py
@@ -0,0 +1,26 @@
+class Account:
+    def __init__(self, id, name, balance=0):
+        self.id = id
+        self.name = name
+        self.balance = balance
+
+    def credit(self, amount):
+        self.balance += amount
+        return self.balance
+
+    def debit(self, amount):
+        if amount > self.balance:
+            return f"Amount exceeded balance"
+        else:
+            self.balance -= amount
+            return self.balance
+
+    def info(self):
+        return f"User {self.name} with account {self.id} has {self.balance} balance"
+
+
+account = Account(5411256, "Peter")
+print(account.debit(500))
+print(account.credit(1000))
+print(account.debit(500))
+print(account.info())
