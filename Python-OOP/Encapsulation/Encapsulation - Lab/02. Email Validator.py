02df564dfa268448f5295947eb7be2559ed77556
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-02-24 22:50:33 +0200

    uploading solutions

---
+++ Python-OOP/Encapsulation/Encapsulation - Lab/02. Email Validator.py
@@ -0,0 +1,26 @@
+class EmailValidator:
+    def __init__(self, min_length, mails, domains):
+        self.min_length = min_length
+        self.mails = mails
+        self.domains = domains
+
+    def validate_name(self, name):
+        return self.min_length >= name
+
+    def __validate_mail(self, mail):
+        return mail in self.mails
+
+    def __validate_domain(self, domain):
+        return domain in self.domains
+
+    def validate(self, email):
+        self.__validate_mail(email)
+
+
+mails = ["gmail", "softuni"]
+domains = ["com", "bg"]
+email_validator = EmailValidator(6, mails, domains)
+print(email_validator.validate("pe77er@gmail.com"))
+print(email_validator.validate("georgios@gmail.net"))
+print(email_validator.validate("stamatito@abv.net"))
+print(email_validator.validate("abv@softuni.bg"))
