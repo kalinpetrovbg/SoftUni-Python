0605ea4aabca875443b69c0918754e83df200387
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-02-27 22:53:11 +0200

    update

--- Python-OOP/01. Defining Classes/05. Programmer.py
+++ Python-OOP/01. Defining Classes/05. Programmer.py
@@ -8,19 +8,16 @@ class Programmer:
         if self.language == language:
             self.skills += skills_earned
             return f"{self.name} watched {course_name}"
-        else:
-            return f"{self.name} does not know {language}"
+        return f"{self.name} does not know {language}"
 
     def change_language(self, new_language, skills_needed):
         if new_language == self.language:
             return f"{self.name} already knows {new_language}"
-        else:
-            if self.skills >= skills_needed:
-                old_language = self.language
-                self.language = new_language
-                return f"{self.name} switched from {old_language} to {new_language}"
-            else:
-                return f"{self.name} needs {skills_needed - self.skills} more skills"
+        if self.skills >= skills_needed:
+            old_language = self.language
+            self.language = new_language
+            return f"{self.name} switched from {old_language} to {new_language}"
+        return f"{self.name} needs {skills_needed - self.skills} more skills"
 
 
 programmer = Programmer("John", "Java", 50)
