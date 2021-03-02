829d5c46484d94c36232a3df32601801b97d0db0
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-03-02 23:11:04 +0200

    update

--- Python-OOP/01. Defining Classes/Exercises/07. Guild System/guild.py
+++ Python-OOP/01. Defining Classes/Exercises/07. Guild System/guild.py
@@ -1,3 +1,35 @@
+from player import Player
+
 class Guild:
-    def __init__(self):
-        pass
+    def __init__(self, name):
+        self.name = name
+        self.players = []
+
+    def assign_player(self, player: Player):
+        if not player.guild == self.name and not player.guild == "Unaffiliated":
+            return f"Player {player.name} is in another guild."
+        filtered = [p for p in self.players if p == player]
+        if filtered:
+            return f"Player {player.name} is already in the guild."
+        self.players.append(player)
+        return f"Welcome player {player.name} to the guild {self.name}"
+
+    def kick_player(self, player_name):
+        if player_name not in [play.name for play in self.players]:
+            return f"Player {player_name} is not in the guild."
+        p = [p for p in self.players if p.name == player_name][0]
+        self.players.remove(p)
+        p.guild = "Unaffiliated"
+        return f"Player {player_name} has been removed from the guild."
+
+    def guild_info(self):
+        result = f"Guild: {self.name}\n{Player.player_info(player)}\n"
+        return result
+
+
+player = Player("George", 50, 100)
+print(player.add_skill("Shield Break", 20))
+print(player.player_info())
+guild = Guild("UGT")
+print(guild.assign_player(player))
+print(guild.guild_info())
