8cc7a17fa12d4189558c0d935a52a6ad63d112a7
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-03-03 22:27:26 +0200

    update

--- Python-OOP/02. Classes and Instances/Exercises/03. Account.py
+++ Python-OOP/02. Classes and Instances/Exercises/03. Account.py
@@ -9,18 +9,16 @@ class Account:
         return self.balance
 
     def debit(self, amount):
-        if amount > self.balance:
-            return f"Amount exceeded balance"
-        else:
+        if amount <= self.balance:
             self.balance -= amount
             return self.balance
+        return "Amount exceeded balance"
 
     def info(self):
         return f"User {self.name} with account {self.id} has {self.balance} balance"
 
 
-account = Account(5411256, "Peter")
-print(account.debit(500))
-print(account.credit(1000))
-print(account.debit(500))
+account = Account(1234, "George", 1000)
+print(account.credit(500))
+print(account.debit(1500))
 print(account.info())
