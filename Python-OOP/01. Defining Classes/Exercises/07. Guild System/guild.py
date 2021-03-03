9aea81a55e6c914457ccfd97ada19b01428defdf
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-03-03 10:52:39 +0200

    update

--- Python-OOP/01. Defining Classes/Exercises/07. Guild System/guild.py
+++ Python-OOP/01. Defining Classes/Exercises/07. Guild System/guild.py
@@ -1,35 +1,28 @@
-from player import Player
-
 class Guild:
     def __init__(self, name):
         self.name = name
         self.players = []
 
-    def assign_player(self, player: Player):
-        if not player.guild == self.name and not player.guild == "Unaffiliated":
-            return f"Player {player.name} is in another guild."
-        filtered = [p for p in self.players if p == player]
-        if filtered:
+    def assign_player(self, player):
+        if player.guild == self.name:
             return f"Player {player.name} is already in the guild."
-        self.players.append(player)
-        return f"Welcome player {player.name} to the guild {self.name}"
+        if player.guild == "Unaffiliated":
+            self.players.append(player)
+            player.guild = self.name
+            return f"Welcome player {player.name} to the guild {self.name}"
+        else:
+            return f"Player {player.name} is in another guild."
 
     def kick_player(self, player_name):
-        if player_name not in [play.name for play in self.players]:
+        try:
+            to_remove = [p for p in self.players if p.name == player_name][0]
+            self.players.remove(to_remove)
+            return f"Player {player_name} has been removed from the guild."
+        except IndexError:
             return f"Player {player_name} is not in the guild."
-        p = [p for p in self.players if p.name == player_name][0]
-        self.players.remove(p)
-        p.guild = "Unaffiliated"
-        return f"Player {player_name} has been removed from the guild."
 
     def guild_info(self):
-        result = f"Guild: {self.name}\n{Player.player_info(player)}\n"
-        return result
-
-
-player = Player("George", 50, 100)
-print(player.add_skill("Shield Break", 20))
-print(player.player_info())
-guild = Guild("UGT")
-print(guild.assign_player(player))
-print(guild.guild_info())
+        res = f"Guild: {self.name}\n"
+        for p in self.players:
+            res += p.player_info()
+        return res
--- Python-OOP/01. Defining Classes/Exercises/07. Guild System/player.py
+++ Python-OOP/01. Defining Classes/Exercises/07. Guild System/player.py
@@ -8,12 +8,12 @@ class Player:
 
     def add_skill(self, skill_name, mana_cost):
         if skill_name in self.skills:
-            return "Skill already added"
+            return f"Skill already added"
         self.skills[skill_name] = mana_cost
         return f"Skill {skill_name} added to the collection of the player {self.name}"
 
     def player_info(self):
-        result = f"Name: {self.name}\nName: {self.guild}\nHP: {self.hp}\nMP: {self.mp}\n"
+        res = f"Name: {self.name}\nGuild: {self.guild}\nHP: {self.hp}\nMP: {self.mp}\n"
         for key, value in self.skills.items():
-            result += f"==={key} - {value}"
-        return result
+            res += f"==={key} - {value}\n"
+        return res
