6939ba51554392bd6a14f3a918247b43b6c9d08a
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-03-06 22:59:31 +0200

    update

---
+++ Python-OOP/02. Classes and Instances/Exercises/07. Library/__init__.py
@@ -0,0 +1,10 @@
+from library import Library
+from user import User
+
+user = User(12, 'Peter')
+library = Library()
+library.add_user(user)
+print(library.add_user(user))
+library.remove_user(user)
+print(library.remove_user(user))
+library.add_user(user)
