c2326a17c1a57aa3f82e6614d2b1171a82a3d0bf
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-03-10 01:04:40 +0200

    update

--- Python OOP/03. Attributes and Methods/Lab/02. Integer.py
+++ Python OOP/03. Attributes and Methods/Lab/02. Integer.py
@@ -1,41 +1,34 @@
-from math import floor
-
-
 class Integer:
-    def __init__(self, value: int):
+    def __init__(self, value):
         self.value = value
 
     @classmethod
-    def from_float(cls, value: float):
-        try:
-            new_value = floor(value)
-            return cls(new_value)
-        except TypeError:
-            return 'value is not a float'
+    def from_float(cls, value):
+        if isinstance(value, float):
+            return cls(int(value))
+        return "value is not a float"
 
     @classmethod
     def from_roman(cls, value):
-        rom_val = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-        int_val = 0
-        for i in range(len(value)):
-            if i > 0 and rom_val[value[i]] > rom_val[value[i - 1]]:
-                int_val += rom_val[value[i]] - 2 * rom_val[value[i - 1]]
+        roman_numerals = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        result = 0
+        for i, c in enumerate(value):
+            if (i + 1) == len(value) or roman_numerals[c] >= roman_numerals[value[i + 1]]:
+                result += roman_numerals[c]
             else:
-                int_val += rom_val[value[i]]
-        return cls(int_val)
+                result -= roman_numerals[c]
+        return result
 
     @classmethod
-    def from_string(cls, value: str):
-        if not isinstance(value, str):
-            return 'wrong type'
-        new_value = int(value)
-        return cls(new_value)
+    def from_string(cls, value):
+        try:
+            x = int(value)
+            return x
+        except:
+            return "wrong type"
 
-    def add(self, other):
-        if not isinstance(other, Integer):
-            return 'number should be an Integer instance'
-        return self.value + other.value
-
+    def add(self, integer):
+        return sum(self.value, integer)
 
 first_num = Integer(10)
 second_num = Integer.from_roman("IV")
