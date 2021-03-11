6f6e935980a785f1aed651afb49485f57d1e1e7f
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-03-11 23:49:14 +0200

    uploading

---
+++ Python OOP/03. Attributes and Methods/Lab/hotel.py
@@ -0,0 +1,6 @@
+class Hotel:
+    def __init__(self, name):
+        self.name = name
+        self.rooms = []
+        self.guests = 0
+        
---
+++ Python OOP/03. Attributes and Methods/Lab/room.py
@@ -0,0 +1,12 @@
+class Room:
+    def __init__(self, number, capacity):
+        self.number = number
+        self.capacity = capacity
+        self.guests = 0
+        self.is_taken = False
+
+    def take_room(self, people):
+        pass
+
+    def free_room(self):
+        pass
