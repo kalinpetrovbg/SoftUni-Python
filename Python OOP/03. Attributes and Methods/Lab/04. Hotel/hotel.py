4ee34f874a268af6c0720e4b61e3bfda86cb5d1f
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-03-11 23:58:39 +0200

    update

--- Python OOP/03. Attributes and Methods/Lab/04. Hotel/hotel.py
+++ Python OOP/03. Attributes and Methods/Lab/04. Hotel/hotel.py
@@ -3,4 +3,22 @@ class Hotel:
         self.name = name
         self.rooms = []
         self.guests = 0
-        
+
+    @classmethod
+    def from_stars(cls, stars_count):
+        pass
+
+    def add_room(self, room):
+        pass
+
+    def take_room(self, room_number, people):
+        pass
+
+    def free_room(self, room_number):
+        pass
+
+    def print_status(self):
+        result = f"Hotel {self.name} has {self.guests} total guests"
+        # result += f"Free rooms:{}"
+        # result += f"Taken rooms:{}"
+        return result
