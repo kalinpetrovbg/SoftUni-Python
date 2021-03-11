90fa2bb735bf86afc4e5fb0e4160c1e7744d7da8
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-03-11 23:24:10 +0200

    uploading solutions

--- Python OOP/03. Attributes and Methods/Lab/02. Integer.py
+++ Python OOP/03. Attributes and Methods/Lab/02. Integer.py
@@ -1,34 +1,41 @@
+import math
+
 class Integer:
     def __init__(self, value):
         self.value = value
 
     @classmethod
     def from_float(cls, value):
-        if isinstance(value, float):
-            return cls(int(value))
+        if type(value) == float:
+            return cls(math.floor(value))
         return "value is not a float"
 
     @classmethod
     def from_roman(cls, value):
-        roman_numerals = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-        result = 0
-        for i, c in enumerate(value):
-            if (i + 1) == len(value) or roman_numerals[c] >= roman_numerals[value[i + 1]]:
-                result += roman_numerals[c]
+        roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000,
+                 'IV': 4, 'IX': 9, 'XL': 40, 'XC': 90, 'CD': 400, 'CM': 900}
+        i = 0
+        num = 0
+        while i < len(value):
+            if i + 1 < len(value) and value[i:i + 2] in roman:
+                num += roman[value[i:i + 2]]
+                i += 2
             else:
-                result -= roman_numerals[c]
-        return result
+                # print(i)
+                num += roman[value[i]]
+                i += 1
+        return cls(num)
 
     @classmethod
     def from_string(cls, value):
-        try:
-            x = int(value)
-            return x
-        except:
+        if type(value) != str:
             return "wrong type"
+        return cls(int(value))
 
     def add(self, integer):
-        return sum(self.value, integer)
+        if type(integer) != Integer:
+            return "number should be an Integer instance"
+        return self.value + integer.value
 
 first_num = Integer(10)
 second_num = Integer.from_roman("IV")
@@ -36,3 +43,4 @@ second_num = Integer.from_roman("IV")
 print(Integer.from_float("2.6"))
 print(Integer.from_string(2.6))
 print(first_num.add(second_num))
+
