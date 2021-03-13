9c29c5ea0796a8da966c15f08b1649a1402826d7
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-03-13 21:39:25 +0200

    upload

---
+++ Python OOP/03. Attributes and Methods/02. Movie World/movie_world.py
@@ -0,0 +1,82 @@
+# from customer import Customer
+# from dvd import DVD
+
+class MovieWorld:
+    def __init__(self, name):
+        self.name = name
+        self.customers = []
+        self.dvds = []
+
+    @staticmethod
+    def dvd_capacity():
+        dvd_capacity = 15
+        return dvd_capacity
+
+    @staticmethod
+    def customer_capacity():
+        customer_capacity = 10
+        return customer_capacity
+
+    def add_customer(self, customer):
+        if len(self.customers) < self.customer_capacity():
+            self.customers.append(customer)
+
+    def add_dvd(self, dvd):
+        if len(self.dvds) < self.dvd_capacity():
+            self.dvds.append(dvd)
+
+    def rent_dvd(self, customer_id, dvd_id):
+        current_customer = [c for c in self.customers if c.id == customer_id][0]
+        current_dvd = [dvd for dvd in self.dvds if dvd.id == dvd_id][0]
+
+        if current_dvd in current_customer.rented_dvds:
+            return f"{current_customer.name} has already rented {current_dvd.name}"
+
+        if current_dvd.is_rented:
+            return "DVD is already rented"
+
+        if current_customer.age <= current_dvd.age_restriction:
+            return f"{current_customer.name} should be at least {current_dvd.age_restriction} to rent this movie"
+
+        else:
+            current_customer.rented_dvds.append(current_dvd)
+            current_dvd.is_rented = True
+            return f"{current_customer.name} has successfully rented {current_dvd.name}"
+
+    def return_dvd(self, customer_id, dvd_id):
+        current_customer = [c for c in self.customers if c.id == customer_id][0]
+        current_dvd = [dvd for dvd in self.dvds if dvd.id == dvd_id][0]
+
+        if current_dvd in current_customer.rented_dvds:
+            current_customer.rented_dvds.remove(current_dvd)
+            current_dvd.is_rented = False
+            return f"{current_customer.name} has successfully returned {current_dvd.name}"
+        return f"{current_customer.name} does not have that DVD"
+
+    def __repr__(self):
+        res = ""
+        for c in self.customers:
+            res += f"{c}\n"
+        for d in self.dvds:
+            res += f"{d}\n"
+        return res
+
+# c1 = Customer("John", 16, 1)
+# c2 = Customer("Anna", 55, 2)
+#
+# d1 = DVD("Black Widow", 1, 2020, "April", 18)
+# d2 = DVD.from_date(2, "The Croods 2", "23.12.2020", 3)
+#
+# movie_world = MovieWorld("The Best Movie Shop")
+#
+# movie_world.add_customer(c1)
+# movie_world.add_customer(c2)
+#
+# movie_world.add_dvd(d1)
+# movie_world.add_dvd(d2)
+#
+# print(movie_world.rent_dvd(1, 1))
+# print(movie_world.rent_dvd(2, 1))
+# print(movie_world.rent_dvd(1, 2))
+#
+# print(movie_world)
