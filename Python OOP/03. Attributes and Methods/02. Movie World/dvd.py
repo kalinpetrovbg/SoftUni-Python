d480135525241d5aa3f68261f322e03ba468c5da
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-03-13 21:50:15 +0200

    uploading solutions

--- Python OOP/03. Attributes and Methods/02. Movie World/dvd.py
+++ Python OOP/03. Attributes and Methods/02. Movie World/dvd.py
@@ -19,4 +19,30 @@ class DVD:
     @classmethod
     def from_date(cls, id, name, date, age_restriction):
         _, creation_month, creation_year = date.split(".")
+        creation_month = int(creation_month)
+        creation_year = int(creation_year)
+        if creation_month == 1:
+            creation_month = "January"
+        elif creation_month == 2:
+            creation_month = "February"
+        elif creation_month == 3:
+            creation_month = "March"
+        elif creation_month == 4:
+            creation_month = "April"
+        elif creation_month == 5:
+            creation_month = "May"
+        elif creation_month == 6:
+            creation_month = "June"
+        elif creation_month == 7:
+            creation_month = "July"
+        elif creation_month == 8:
+            creation_month = "August"
+        elif creation_month == 9:
+            creation_month = "September"
+        elif creation_month == 10:
+            creation_month = "October"
+        elif creation_month == 11:
+            creation_month = "November"
+        elif creation_month == 12:
+            creation_month = "December"
         return cls(name, id, creation_year, creation_month, age_restriction)
