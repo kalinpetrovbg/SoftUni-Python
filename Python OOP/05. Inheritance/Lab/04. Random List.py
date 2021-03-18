71467ff93ff7adc6222922cb1c199e8074668087
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-03-18 22:54:22 +0200

    uploading solutions

---
+++ Python OOP/05. Inheritance/Lab/04. Random List.py
@@ -0,0 +1,14 @@
+from random import random
+
+class RandomList(list):
+    def __init__(self):
+        pass
+
+    def remove_element(self):
+        pass
+
+    @staticmethod
+    def get_random_element():
+        pass
+
+
