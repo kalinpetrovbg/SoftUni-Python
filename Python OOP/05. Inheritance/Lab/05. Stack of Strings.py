ad5488402cc968a7ff4ee64f98b09f589ee75fad
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-03-18 23:00:37 +0200

    uploading solutions

---
+++ Python OOP/05. Inheritance/Lab/05. Stack of Strings.py
@@ -0,0 +1,18 @@
+class Stack:
+    def __init__(self):
+        self.data = []
+
+    def push(self, item):
+        self.data.append(item)
+
+    def pop(self):
+        return self.data.pop()
+
+    def peek(self):
+        return self.data[-1]
+
+    def is_empty(self):
+        return len(self.data) == 0
+
+    def __str__(self):
+        return self.data
