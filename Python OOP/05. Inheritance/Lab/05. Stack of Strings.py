55ff3f9a4988861fe8c6e89f38f2f4913edcdf32
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-03-18 23:19:29 +0200

    update

--- Python OOP/05. Inheritance/Lab/05. Stack of Strings.py
+++ Python OOP/05. Inheritance/Lab/05. Stack of Strings.py
@@ -15,25 +15,4 @@ class Stack:
         return len(self.data) == 0
 
     def __str__(self):
-        return f"[{','.join(self.data)}]"
-
-
-# test zero
-import unittest
-
-
-class StackTests(unittest.TestCase):
-    def test_zero(self):
-        stack = Stack()
-        stack.push("apple")
-        stack.push("carrot")
-        self.assertEqual(str(stack), '[carrot, apple]')
-        self.assertEqual(stack.pop(), 'carrot')
-        self.assertEqual(stack.peek(), 'apple')
-        stack.push("cucumber")
-        self.assertEqual(str(stack), '[cucumber, apple]')
-        self.assertEqual(stack.is_empty(), False)
-
-
-if __name__ == '__main__':
-    unittest.main()
+        return f"[{', '.join(reversed(self.data))}]"
