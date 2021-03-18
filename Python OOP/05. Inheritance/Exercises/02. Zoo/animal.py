bb4b9164c8c4c2513f1cc31c283af3daa11d8cee
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-03-18 23:38:56 +0200

    uploading solutions

---
+++ Python OOP/05. Inheritance/Exercises/02. Zoo/animal.py
@@ -0,0 +1,11 @@
+class Animal:
+    def __init__(self, name):
+        self.__name = name
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, new_name):
+        self.__name = new_name
