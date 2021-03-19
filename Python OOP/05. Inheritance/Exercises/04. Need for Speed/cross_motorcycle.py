985831823878b6669d0fff2d83e14dd40def7e45
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-03-19 18:30:37 +0200

    uploading solutions

---
+++ Python OOP/05. Inheritance/Exercises/04. Need for Speed/cross_motorcycle.py
@@ -0,0 +1,5 @@
+from project.motorcycle import Motorcycle
+
+class CrossMotorcycle(Motorcycle):
+    def __init__(self, fuel, horse_power):
+        super().__init__(fuel, horse_power)
---
+++ Python OOP/05. Inheritance/Exercises/04. Need for Speed/family_car.py
@@ -0,0 +1,5 @@
+from project.car import Car
+
+class FamilyCar(Car):
+    def __init__(self, fuel, horse_power):
+        super().__init__(fuel, horse_power)
