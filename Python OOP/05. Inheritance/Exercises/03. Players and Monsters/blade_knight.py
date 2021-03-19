2a8f19fc1cb4cac952cee06408c5f4f959f97e3b
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-03-19 17:13:49 +0200

    uploading solutions

---
+++ Python OOP/05. Inheritance/Exercises/03. Players and Monsters/blade_knight.py
@@ -0,0 +1,5 @@
+from project.dark_knight import DarkKnight
+
+class BladeKnight(DarkKnight):
+    def __init__(self, username, level):
+        super().__init__(username, level)
---
+++ Python OOP/05. Inheritance/Exercises/03. Players and Monsters/dark_knight.py
@@ -0,0 +1,5 @@
+from project.knight import Knight
+
+class DarkKnight(Knight):
+    def __init__(self, username, level):
+        super().__init__(username, level)
