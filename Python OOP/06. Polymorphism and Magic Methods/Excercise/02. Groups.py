81819c32f45b5a87d18652628c353ab238287eb4
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-03-21 14:10:02 +0200

    update

--- Python OOP/06. Polymorphism and Magic Methods/Excercise/02. Groups.py
+++ Python OOP/06. Polymorphism and Magic Methods/Excercise/02. Groups.py
@@ -3,19 +3,29 @@ class Person:
         self.name = name
         self.surname = surname
 
-    def full_name(self):
-        return self.name + " " + self.surname
-
     def __add__(self, other):
-        return self.full_name() + other.full_name()
+        return Person(self.name, other.surname)
+
+    def __str__(self):
+        return f"{self.name} {self.surname}"
 
 class Group:
     def __init__(self, name, people):
         self.name = name
         self.people = list(people)
 
+    def __add__(self, other):
+        return Group(self.name + other.name, self.people + other.people)
 
+    def __len__(self):
+        return len(self.people)
 
+    def __repr__(self):
+        people = [str(p) for p in self.people]
+        return f"Group {self.name} with members {', '.join(people)}"
+
+    def __getitem__(self, item):
+        return f"Person {item}: {self.people[item]}"
 
 
 p0 = Person('Aliko', 'Dangote')
@@ -24,10 +34,12 @@ p2 = Person('Warren', 'Buffet')
 p3 = Person('Elon', 'Musk')
 p4 = p2 + p3
 
-# first_group = Group('__VIP__', [p0, p1, p2])
-# # second_group = Group('Special', [p3, p4])
-# # third_group = first_group + second_group
-#
-# print(len(first_group))
-# print(second_group)
-# # print(third_group[0])
+first_group = Group('__VIP__', [p0, p1, p2])
+second_group = Group('Special', [p3, p4])
+third_group = first_group + second_group
+
+print(len(first_group))
+print(second_group)
+print(third_group[0])
+for person in third_group:
+    print(person)
