78519ea99c552b709e36918b6cf8b8346544b81b
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-03-21 23:44:19 +0200

    update

--- Python OOP/07. Solid/Lab/02. Animal.py
+++ Python OOP/07. Solid/Lab/02. Animal.py
@@ -1,21 +1,22 @@
 class Animal:
-    def __init__(self, species):
-        self.species = species
+    pass
 
-    def get_species(self):
-        return self.species
+class Cat(Animal):
+    def animal_sound(self):
+        return 'meow'
 
-
-def animal_sound(animals: list):
-    for animal in animals:
-        if animal.species == 'cat':
-            print('meow')
-        elif animal.species == 'dog':
-            print('woof-woof')
+class Dog(Animal):
+    def animal_sound(self):
+        return 'woof-woof'
 
 
-animals = [Animal('cat'), Animal('dog')]
-animal_sound(animals)
+def animal_sound(animals):
+    for a in animals:
+        return a.animal_sound()
+
+
+animals = [Cat(), Dog()]
+print(animal_sound(animals))
 
 ## добавете ново животно и рефакторирайте кода да работи без да се налага да се правят промени по него
 ## при добавяне на нови животни
