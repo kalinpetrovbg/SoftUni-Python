d34bc51f1a83ddca84951f8c24152002ea50f599
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-03-22 23:12:34 +0200

    update

--- Python OOP/07. Solid/Lab/02. Animal.py
+++ Python OOP/07. Solid/Lab/02. Animal.py
@@ -1,10 +1,15 @@
-class Animal:
-        def __init__(self, species):
-            self.species = species
+from abc import ABC, abstractmethod
 
-        def get_species(self):
-            return self.species
+class Animal(ABC):
+    def __init__(self, species):
+        self.species = species
 
+    def get_species(self):
+        return self.species
+
+    @abstractmethod
+    def make_sound(self):
+        pass
 
 class Cat(Animal):
     def make_sound(self):
@@ -14,10 +19,13 @@ class Dog(Animal):
     def make_sound(self):
         return 'woof-woof'
 
+class Chicken(Animal):
+    def make_sound(self):
+        return "chick-chirik"
 
-def animal_sound(animals):
-    for a in animals:
-        a.make_sound()
+def animal_sound(animals: list[Animal]):
+    for animal in animals:
+        print(animal.make_sound())
 
 
 animals = [Animal('cat'), Animal('dog'), Animal('chicken')]
