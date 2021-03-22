4a5fe6e659eb276a472571b40eadb07eff607ff2
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-03-22 23:57:46 +0200

    uploading solutions

--- Python OOP/07. Solid/Lab/03. Robots.py
+++ Python OOP/07. Solid/Lab/03. Robots.py
@@ -1,3 +1,5 @@
+from abc import ABC, abstractmethod
+
 class Robot:
     def __init__(self, type):
         self.type = type
@@ -5,24 +7,24 @@ class Robot:
     def get_type(self):
         return self.type
 
+    @staticmethod
+    def count_senzors():
+        pass
+
 
 class Android(Robot):
-    def android_senzors_count(self):
+    def count_senzors(self):
         return 4
 
 
 class Chappie(Robot):
-    def chappie_senzors_count(self):
+    def count_senzors(self):
         return 6
 
 
 def count_robot_senzors(robots: list):
     for robot in robots:
-        if isinstance(robot, Android):
-            print(robot.android_senzors_count())
-        elif isinstance(robot, Chappie):
-            print(robot.chappie_senzors_count())
-
+        print(robot.count_senzors())
 
 robots = [Android('Robocop'), Chappie('XIX')]
 count_robot_senzors(robots)
