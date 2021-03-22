34cb45a577f92a58d47caf4c92fb73ec0d2dc141
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-03-22 17:13:21 +0200

    update

--- Python OOP/07. Solid/Lab/01. Books.py
+++ Python OOP/07. Solid/Lab/01. Books.py
@@ -3,13 +3,25 @@ class Book:
         self.title = title
         self.author = author
 
+
+class BookReader:
+    def __init__(self, book):
+        self.book = book
+        self.page = 0
+
+    def change_page(self, page):
+        self.page = page
+
+
 class Library:
     def __init__(self, books):
         self.books = list(books)
 
     def find_book(self, title):
-        found = [b for b in self.books if b.title == title][0]
-        return f"Book found: {found.title}"
+        found = [book for book in self.books if b.title == title][0]
+        if found:
+            return f"Book found: {found.title}"
+        return "The Book was not found!"
 
     def add_book(self, book):
         self.books.append(book)
