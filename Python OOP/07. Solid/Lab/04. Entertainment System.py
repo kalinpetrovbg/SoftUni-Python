50b8259b52f6ca197472a1dc34b3510987451521
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-03-23 19:28:01 +0200

    update

--- Python OOP/07. Solid/Lab/04. Entertainment System.py
+++ Python OOP/07. Solid/Lab/04. Entertainment System.py
@@ -1,46 +1,37 @@
-class EntertainmentDevice:
-    def connect_to_device_via_hdmi_cable(self, device): pass
-    def connect_to_device_via_rca_cable(self, device): pass
-    def connect_to_device_via_ethernet_cable(self, device): pass
-    def connect_device_to_power_outlet(self, device): pass
+class HDMI:
+    def connect(self, other):
+        return f"{self} is connected with {other}"
 
+class Ethernet:
+    def connect(self, other):
+        return f"{self} is connected with {other}"
 
-class Television(EntertainmentDevice):
-    def connect_to_dvd(self, dvd_player):
-        self.connect_to_device_via_rca_cable(dvd_player)
+class ACDCPower:
+    def connect(self, other):
+        return f"{self} is connected with {other}"
 
-    def connect_to_game_console(self, game_console):
-        self.connect_to_device_via_hdmi_cable(game_console)
+class Television:
+    def __init__(self):
+        self.connect_hdmi = HDMI()
+        self.connect_power = ACDCPower()
 
-    def plug_in_power(self):
-        self.connect_device_to_power_outlet(self)
+class DVDPlayer:
+    def __init__(self):
+        self.connect_hdmi = HDMI()
+        self.connect_power = ACDCPower()
 
+class GameConsole:
+    def __init__(self):
+        self.connect_ethernet = Ethernet()
+        self.connect_hdmi = HDMI()
+        self.connect_power = ACDCPower()
 
-class dvd_player(EntertainmentDevice):
-    def connect_to_tv(self, television):
-        self.connect_to_device_via_hdmi_cable(television)
+class Router:
+    def __init__(self):
+        self.connect_ethernet = Ethernet()
+        self.connect_power = ACDCPower()
 
-    def plug_in_power(self):
-        self.connect_device_to_power_outlet(self)
+tv = Television()
+DVD = DVDPlayer()
 
-
-class GameConsole(EntertainmentDevice):
-    def connect_to_tv(self, television):
-        self.connect_to_device_via_hdmi_cable(television)
-
-    def connect_to_router(self, router):
-        self.connect_to_device_via_ethernet_cable(router)
-
-    def plug_in_power(self):
-        self.connect_device_to_power_outlet(self)
-
-
-class Router(EntertainmentDevice):
-    def connect_to_tv(self, television):
-        self.connect_to_device_via_ethernet_cable(television)
-
-    def connect_to_game_console(self, game_console):
-        self.connect_to_device_via_ethernet_cable(game_console)
-
-    def plug_in_power(self):
-        self.connect_device_to_power_outlet(self)
+print(tv.connect_power.connect(DVD.connect_power))
