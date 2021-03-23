4e99ca77967afcb9066e6c48482009bc8cf4ebc1
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-03-23 19:48:31 +0200

    uploading solutions

--- Python OOP/07. Solid/Lab/05. Print Books.py
+++ Python OOP/07. Solid/Lab/05. Print Books.py
@@ -1,15 +1,31 @@
+from abc import ABC, abstractmethod
+
 class Book:
-    def __init__(self, content: str):
+    def __init__(self, name, author, content):
+        self.name = name
+        self.author = author
         self.content = content
 
+class Formatter(ABC):
+    @abstractmethod
+    def format(self, book: Book):
+        pass
 
-class Formatter:
+class ContentFormatter(Formatter):
     def format(self, book: Book) -> str:
         return book.content
 
+class AuthorFormatter(Formatter):
+    def format(self, book: Book) -> str:
+        return book.author
+
 
 class Printer:
-    def get_book(self, book: Book):
-        formatter = Formatter()
-        formatted_book = formatter.format(book)
-        return formatted_book
+    def print(self, book: Book, formatter: Formatter):
+        return formatter.format(book)
+
+book = Book("Title of the book", "John Levine", "Content of the book")
+printer = Printer()
+
+print(printer.print(book, AuthorFormatter()))
+print(printer.print(book, ContentFormatter()))
