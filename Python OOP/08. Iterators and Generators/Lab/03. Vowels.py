4dde9e225313e46f01f9e85d35e5c3802df5479d
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-03-24 23:50:27 +0200

    uploading semi-solution

---
+++ Python OOP/08. Iterators and Generators/Lab/03. Vowels.py
@@ -0,0 +1,20 @@
+class vowels:
+    def __init__(self, string):
+        self.string = string
+        self.index = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.index > len(self.string) - 1:
+            raise StopIteration
+        current_letter = self.string[self.index]
+        self.index += 1
+        if current_letter.lower() in "aeoiyu":
+            return current_letter
+
+
+my_string = vowels('Abcedifuty0o')
+for char in my_string:
+    print(char)
