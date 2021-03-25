fd0a304c52e14cab2a78d3180048fdc2896ba095
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-03-25 18:51:22 +0200

    uploading solutions

---
+++ Python OOP/08. Iterators and Generators/Exercise/08. Prime Numbers.py
@@ -0,0 +1,12 @@
+def get_primes(ll):
+    for each in ll:
+        if each < 2:
+            continue
+        is_prime = True
+        for i in range(2, each):
+            if each % i == 0:
+                is_prime = False
+        if is_prime:
+            yield each
+
+print(list(get_primes([2, 4, 3, 5, 6, 9, 1, 0])))
