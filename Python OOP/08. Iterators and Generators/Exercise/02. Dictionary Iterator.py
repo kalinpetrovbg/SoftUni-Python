b5fe0de07a8720904d5513eeb613028c306dff4e
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-03-25 15:00:23 +0200

    update

--- Python OOP/08. Iterators and Generators/Exercise/02. Dictionary Iterator.py
+++ Python OOP/08. Iterators and Generators/Exercise/02. Dictionary Iterator.py
@@ -1,6 +1,6 @@
 class dictionary_iter:
     def __init__(self, dictionary: dict):
-        self.dictionary = [(key, value) for key, value in dictionary.items()]
+        self.dictionary = dictionary
         self.index = 0
 
     def __iter__(self):
@@ -9,9 +9,10 @@ class dictionary_iter:
     def __next__(self):
         if self.index == len(self.dictionary):
             raise StopIteration
-        current = self.dictionary[self.index]
+        key = list(self.dictionary.keys())[self.index]
+        value = self.dictionary[key]
         self.index += 1
-        return current
+        return key, value
 
 
 result = dictionary_iter({1: "1", 2: "2"})
