4072805ddc622fffcca25ab0443eb8b3f5343c40
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-03-26 23:33:52 +0200

    uploading solutions

---
+++ Python OOP/09. Decorators/Exercise/04. Type Check.py
@@ -0,0 +1,27 @@
+def type_check(value):
+    def inner(func):
+        def wrapper(*args):
+            for a in args:
+                if type(a) == value:
+                    continue
+                else:
+                    return "Bad Type"
+            result = func(*args)
+            return result
+        return wrapper
+    return inner
+
+
+@type_check(int)
+def times2(num):
+    return num*2
+print(times2(2))
+print(times2('Not A Number'))
+
+
+@type_check(str)
+def first_letter(word):
+    return word[0]
+
+print(first_letter('Hello World'))
+print(first_letter(['Not', 'A', 'String']))
