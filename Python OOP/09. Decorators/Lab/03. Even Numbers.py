e35f67653bd4391c50521cb5691eecd861f5726d
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-03-26 20:02:15 +0200

    uploading solutions

---
+++ Python OOP/09. Decorators/Lab/03. Even Numbers.py
@@ -0,0 +1,11 @@
+def even_numbers(function):
+    def wrapper(*args):
+        result = function(*args)
+        return [x for x in result if x % 2 == 0]
+    return wrapper
+
+@even_numbers
+def get_numbers(numbers):
+    return numbers
+
+print(get_numbers([1, 2, 3, 4, 5]))
