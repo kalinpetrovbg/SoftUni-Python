e68283921b3d91dad78d6bb2aabd64c4f16f3a80
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-04-01 23:09:18 +0300

    fix

--- Python OOP/Exams/Bunker Game/medicine/salve.py
+++ Python OOP/Exams/Bunker Game/medicine/salve.py
@@ -1,18 +1,6 @@
 from project.medicine.medicine import Medicine
-from abc import ABC, abstractmethod
-from project.survivor import Survivor
 
 
-class Salve(Medicine, ABC):
+class Salve(Medicine):
     def __init__(self):
         super().__init__(health_increase=50)
-
-    @property
-    def health_increase(self):
-        return self.__health_increase
-
-    @abstractmethod
-    def apply(self, survivor: Survivor):
-        survivor.health += self.__health_increase
-        if survivor.health > 100:
-            survivor.health = 100
