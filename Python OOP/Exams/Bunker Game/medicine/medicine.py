3aca637b1a80f4607286e1855eb0b40fdca3447d
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-04-01 23:08:43 +0300

    fix

--- Python OOP/Exams/Bunker Game/medicine/medicine.py
+++ Python OOP/Exams/Bunker Game/medicine/medicine.py
@@ -1,19 +1,21 @@
-from abc import ABC, abstractmethod
+from abc import ABC
 from project.survivor import Survivor
 
 
 class Medicine(ABC):
     def __init__(self, health_increase):
         self.__health_increase = health_increase
-        if self.__health_increase < 0:
-            raise ValueError("Health increase cannot be less than zero.")
 
     @property
     def health_increase(self):
         return self.__health_increase
 
-    @abstractmethod
+    @health_increase.setter
+    def health_increase(self, value):
+        if value < 0:
+            raise ValueError("Health increase cannot be less than zero.")
+        self.__health_increase = value
+
     def apply(self, survivor: Survivor):
         survivor.health += self.__health_increase
-        if survivor.health > 100:
-            survivor.health = 100
+
