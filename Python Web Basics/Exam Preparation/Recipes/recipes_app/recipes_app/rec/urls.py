2a5507ce71c16c2f8744dd75c1631a0edbfa6a64
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-06-20 15:04:27 +0300

    added forgotten functionality for editing the recipes

--- Python Web Basics/Exam Preparation/Recipes/recipes_app/recipes_app/rec/urls.py
+++ Python Web Basics/Exam Preparation/Recipes/recipes_app/recipes_app/rec/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from recipes_app.rec.views import home_page, create_recipe, \
-    edit_recipe, delete_recipe, details, create_new, delete_item
+    edit_recipe, delete_recipe, details, create_new, delete_item, edit_btn
 
 urlpatterns = [
     path('', home_page, name='home'),
     path('create/', create_recipe, name='create'),
     path('edit/<int:pk>', edit_recipe, name='edit'),
+    path('ed/<int:pk>', edit_btn, name='edit_btn'),
     path('delete/<int:pk>', delete_recipe, name='delete'),
     path('del/<int:pk>', delete_item, name='del'),
     path('details/<int:pk>', details, name='details'),
--- Python Web Basics/Exam Preparation/Recipes/recipes_app/recipes_app/rec/views.py
+++ Python Web Basics/Exam Preparation/Recipes/recipes_app/recipes_app/rec/views.py
@@ -10,8 +10,10 @@ def home_page(request):
     }
     return render(request, 'index.html', context)
 
+
 def create_recipe(request):
     return render(request, 'create.html')
+
 
 def create_new(request):
     title = request.POST['title']
@@ -24,8 +26,42 @@ def create_new(request):
     form.save()
     return redirect('/')
 
+
 def edit_recipe(request, pk):
-    return render(request, 'edit.html')
+    current = Recipe.objects.get(pk=pk)
+    context = {
+        'recipe': current,
+    }
+    return render(request, 'edit.html', context)
+
+
+def edit_btn(request, pk):
+    current = Recipe.objects.get(pk=pk)
+
+    title = request.POST['title']
+    image_url = request.POST['image_url']
+    description = request.POST['description']
+    ingredients = request.POST['ingredients']
+    time = request.POST['time']
+
+    form = Recipe(title=title, image_url=image_url, description=description, ingredients=ingredients, time=time)
+
+    if form.title == current.title and \
+            form.image_url == current.image_url and\
+            form.description == current.description and \
+            form.ingredients == current.ingredients and \
+            form.time == current.time:
+        form.save()
+    else:
+        current.title = title
+        current.image_url = image_url
+        current.description = description
+        current.ingredients = ingredients
+        current.time = time
+        current.save()
+
+    return redirect('/')
+
 
 def delete_recipe(request, pk):
     current = Recipe.objects.get(pk=pk)
@@ -34,11 +70,11 @@ def delete_recipe(request, pk):
     }
     return render(request, 'delete.html', context)
 
+
 def delete_item(request, pk):
     current = Recipe.objects.get(pk=pk)
     current.delete()
     return redirect('/')
-
 
 
 def details(request, pk):
