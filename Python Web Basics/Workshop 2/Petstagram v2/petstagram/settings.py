fa3987a0ef8821fefac3f99b8c538966ca6009e5
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-06-23 10:54:04 +0300

    Update settings.py
    remove secret key

--- Python Web Basics/Workshop 2/Petstagram v2/petstagram/settings.py
+++ Python Web Basics/Workshop 2/Petstagram v2/petstagram/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@bx6!#&y_erz)&4&6@uftef1=+n1f@n(2_k%98s86-n5_sunb9'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
