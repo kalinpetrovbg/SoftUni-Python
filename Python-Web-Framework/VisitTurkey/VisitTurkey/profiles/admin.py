33f5dd24dd545744676590af7de5b22ed3c22236
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-08-10 23:52:29 +0300

    update admin views

--- Python-Web-Framework/VisitTurkey/VisitTurkey/profiles/admin.py
+++ Python-Web-Framework/VisitTurkey/VisitTurkey/profiles/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from VisitTurkey.profiles.models import Profile
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'first_name', 'last_name', 'age', 'city']
+    search_fields = ['first_name', 'last_name']
+    readonly_fields = ['first_name', 'last_name', 'user']
+
+
+admin.site.register(Profile, ProfileAdmin)
--- Python-Web-Framework/VisitTurkey/VisitTurkey/website/admin.py
+++ Python-Web-Framework/VisitTurkey/VisitTurkey/website/admin.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
+
 from VisitTurkey.website.models import Place
 
+
+@admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
-    list_display = ('name', 'location', 'type', 'description')
-    # list_filter = ('type',)
+    list_display = ('name', 'location', 'user')
+    search_fields = ['name']
+    readonly_fields = ['user']
 
-admin.site.register(Place)
 
-
+# admin.site.register(Place)
