729b6dcbdaec94f30ebfe0be1b3a0309b3258e9e
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-08-13 02:11:23 +0300

    profile view fix

--- Python-Web-Framework/VisitTurkey/VisitTurkey/profiles/views.py
+++ Python-Web-Framework/VisitTurkey/VisitTurkey/profiles/views.py
@@ -8,15 +8,7 @@ from VisitTurkey.profiles.models import Profile
 @login_required
 def profile_info(request):
     profile = Profile.objects.get(pk=request.user.id)
-    if request.method == 'POST':
-        form = ProfileForm(request.POST, instance=profile)
-        if form.is_valid():
-            form.save()
-            return redirect('profile info')
-    else:
-        form = ProfileForm(instance=profile)
-
-    context = {'form': form}
+    context = {'profile': profile}
 
     return render(request, 'accounts/profile.html', context)
 
