50c726f163b76d14e38063dbd021309a8abfda00
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-08-14 13:30:01 +0300

    add postgres

--- Python-Web-Framework/VisitTurkey/VisitTurkey/settings.py
+++ Python-Web-Framework/VisitTurkey/VisitTurkey/settings.py
@@ -67,13 +67,23 @@ WSGI_APPLICATION = 'VisitTurkey.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'turkey',
+        'USER': 'postgres',
+        'PASSWORD': 'KA@pos86',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
